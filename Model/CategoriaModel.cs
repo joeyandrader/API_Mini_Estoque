cb6d95cf0176e38d80789d3cc403cf4c409737be
Joel Andrade <joel.a.rocha@hotmail.com>
2022-08-25 09:50:13 -0300

    Iniciando projeto mini estoque!

---
+++ Model/CategoriaModel.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Model
+{
+    public class CategoriaModel
+    {
+        public int CategoriaId { get; set; }
+        public string? Nome { get; set; }
+        public string? ImageUrl { get; set; }
+        
+
+    }
+}
